055dd205fe52b5ed3bda37239957abecbe976dd9
Axl1Huang <AxlHuangFI@outlook.com>
2020-10-28 12:54:23 +0200

    28/10

--- lecture1.py
+++ lecture1.py
@@ -1,7 +1,13 @@
-def function_with_return(input_to_function1,input_to_function2):
-  variable_that_we_return_from_function =input_to_function1+input_to_function2
-  return variable_that_we_return_from_function
+#! /usr/bin/python3  
+def function_with_return(input_to_function1,input_to_function2,input_to_function3):
+    variable_that_we_return_from_function=input_to_function1 + input_to_function2+ input_to_function3
+    return variable_that_we_return_from_function
 
-  answer_to_print1= function_with_return(1,4)
+answer_to_print1= function_with_return(1,4,5)
+def function_with_return2(input_to_function1,input_to_function2,input_to_function3):
+    variable_that_we_return_from_function=input_to_function1 + input_to_function2+ input_to_function3
+    return variable_that_we_return_from_function
 
-  print(answer_to_print1)
+answer_to_print2= function_with_return2(1,2,3)
+print(answer_to_print1)
+print(answer_to_print2)
---
+++ use_function_from_file.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python3
+from list_sum_function import list_to_sum
+
+ans=list_to_sum([1,2,3,4,5,6,7])
+print(ans)
